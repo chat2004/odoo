bd26b351cff648653c31c86d64dd8e14e2fff8f5
Vu Huynh <vu.huynh@kulturit.no>
2017-06-01 14:47:47 +0700

    Remove the test module

--- ext_addons/legacy_projects/models/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class legacy_projects(models.Model):
-    _inherit = 'project.project'
-
-    legacy_projects_id = fields.Integer(default=0, string='Legacy project ID')
